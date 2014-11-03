6ca228dc70ba798207bf78b9a85e242be9aa49d6
Kunal Mehta <legoktm@gmail.com>
2014-11-03 10:53:59 -0800

    Use requests.Session() to keep the connection open

--- phabricator/__init__.py
+++ phabricator/__init__.py
@@ -11,9 +11,10 @@ class Phabricator:
         self.host = host
         self.user = user
         self.cert = cert
-        self.session = {}
+        self.phab_session = {}
         self.token = int(time.time())
         self.signature = hashlib.sha1(str(self.token) + self.cert).hexdigest()
+        self.req_session = requests.Session()
 
     @property
     def connect_params(self):
@@ -28,7 +29,7 @@ class Phabricator:
         }
 
     def connect(self):
-        req = requests.post('%s/api/conduit.connect' % self.host, data={
+        req = self.req_session.post('%s/api/conduit.connect' % self.host, data={
             'params': json.dumps(self.connect_params),
             'output': 'json',
             '__conduit__': True,
@@ -36,7 +37,7 @@ class Phabricator:
 
         # Parse out the response (error handling ommitted)
         result = json.loads(req.content)['result']
-        self.session = {
+        self.phab_session = {
             'sessionKey': result['sessionKey'],
             'connectionID': result['connectionID'],
         }
@@ -44,11 +45,11 @@ class Phabricator:
     def request(self, method, params=None):
         if params is None:
             params = {}
-        if not self.session:
+        if not self.phab_session:
             self.connect()
         url = '%s/api/%s' % (self.host, method)
-        params['__conduit__'] = self.session
-        req = requests.post(url, data={
+        params['__conduit__'] = self.phab_session
+        req = self.req_session.post(url, data={
             'params': json.dumps(params),
             'output': 'json',
         })
