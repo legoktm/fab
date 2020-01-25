1ea57bf978d7355df7fc8c1a1ff449fe708dadc8
Merlijn van Deen <valhallasw@arctus.nl>
2020-01-25 13:45:12 +0100

    Set up basic config scaffold

--- tests/test_fab.py
+++ tests/test_fab.py
@@ -1,2 +1,9 @@
+from pathlib import Path
+import json
+
+root = Path(__file__).parent.parent
+config = json.load((root / "test_config.json").open(encoding="utf-8"))
+
+
 def test_true():
     assert True
