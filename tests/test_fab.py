24b677669074d20dd8dae15724be0330d92fa28c
Merlijn van Deen <valhallasw@arctus.nl>
2020-01-25 13:56:53 +0100

    Add fab tests

--- tests/test_fab.py
+++ tests/test_fab.py
@@ -1,9 +1,36 @@
 from pathlib import Path
 import json
+import pytest
+
+import phabricator
 
 root = Path(__file__).parent.parent
 config = json.load((root / "test_config.json").open(encoding="utf-8"))
 
 
-def test_true():
-    assert True
+def test_token():
+    phab = phabricator.Phabricator(config['host'], config['username'], token=config['token'])
+
+    result = phab.request('user.whoami')
+
+    assert result['userName'] == config['username']
+
+
+def test_certificate():
+    """Deprecated certificate-based authentication"""
+    phab = phabricator.Phabricator(config['host'], config['username'], cert=config['certificate'])
+
+    result = phab.request('user.whoami')
+
+    assert result['userName'] == config['username']
+
+
+def test_error():
+    phab = phabricator.Phabricator(config['host'], config['username'], token=config['token'])
+
+    try:
+        phab.request('user.transactions')
+        pytest.fail()
+    except phabricator.PhabricatorException as e:
+        assert e.error_code == "ERR-CONDUIT-CORE"
+        assert e.error_info == "Only admins can call this API"
